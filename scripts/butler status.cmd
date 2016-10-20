1219b9d96dfc92c32143f8d739f6bad3074f0549
narF <narF.snw@gmail.com>
2016-10-20 00:03:02 -0400

    script for butler (works on my machine only)

---
+++ scripts/butler status.cmd
@@ -0,0 +1,5 @@
+cd C:\Users\narF\Desktop\It's Raining Cabbages
+
+butler status narf/its-raining-cabbages
+
+pause
---
+++ scripts/push PC version to itchio.cmd
@@ -0,0 +1,25 @@
+set dossier="C:\Users\narF\Desktop\It's Raining Cabbages"
+
+set current=%cd%
+echo %current%
+
+:: set /p version=Numero de version? (avec un 'v' devant): 
+
+
+cd %dossier%\win32
+rename nw.exe cabbages.exe
+cd ..
+
+copy %appdata%\itch\bin\butler.exe .
+
+butler push --userversion-file="%current%\version-pc.txt" win32 narf/its-raining-cabbages:windows
+
+butler push --userversion-file="%current%\version-pc.txt" osx64 narf/its-raining-cabbages:mac-osx
+
+butler push --userversion-file="%current%\version-pc.txt" linux32 narf/its-raining-cabbages:linux32
+
+butler push --userversion-file="%current%\version-pc.txt" linux64 narf/its-raining-cabbages:linux64
+
+butler status narf/its-raining-cabbages
+
+pause
